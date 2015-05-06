c519750ccebfde6a2381dce134d7f9774ea49372
Benjamin Laugueux <benjamin@yzalis.com>
2015-05-06 08:36:59 +0200

    Improve code quality.

--- src/Supervisor/Supervisor.php
+++ src/Supervisor/Supervisor.php
@@ -40,7 +40,7 @@ class Supervisor
 
         $this->rpcClient = new RpcClient('http://'.$ipAddress.':'.$port.'/RPC2/');
 
-        if ($username != null && $password != null) {
+        if ($username !== null && $password !== null) {
             $this->rpcClient->getHttpClient()->setAuth($username, $password, HttpClient::AUTH_BASIC);
         }
 
@@ -117,7 +117,7 @@ class Supervisor
 
     /**
      * Check if supervisor is running
-     * 
+     *
      * @return boolean
      */
     public function checkConnection()
